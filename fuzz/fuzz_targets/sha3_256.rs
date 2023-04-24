e07442223a48ac7167f146e32d320ccd6d214616
Matthew Jurenka <matt.jurenka@comcast.net>
2023-04-24 19:21:56 +0000

    adding hash fuzz targets

---
+++ fuzz/fuzz_targets/sha3_256.rs
@@ -0,0 +1,13 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use fastcrypto::hash::*;
+
+fuzz_target!(|value: &[u8]| {
+    let digest1 = Sha3_256::digest(value);
+
+    let mut hash_function = Sha3_256::default();
+    hash_function.update(value);
+    let digest2 = hash_function.finalize();
+
+    assert_eq!(digest1, digest2);
+});
